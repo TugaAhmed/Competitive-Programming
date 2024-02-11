87a47434316ee470767ebb89cde2c926036186db
TugaAhmed <tugakariem@gmail.com>
2024-02-12 00:37:43 +0300

    decreasing array

---
+++ Non-decreasing Array.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/non-decreasing-array/
+
+
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count = 0
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                count += 1
+                if count > 1:
+                    return False
+                if i > 0 and nums[i + 1] < nums[i - 1]:
+                    nums[i + 1] = nums[i]
+                else:
+                    nums[i] = nums[i + 1]
+        return True
+
