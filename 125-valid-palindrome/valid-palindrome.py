aed7f1fbaed87af34d7f541aa1be20b52dc52ecf
Tuga Yousif <tugakariem@gmail.com>
2025-02-19 16:31:26 +0300

    Time: 195 ms (6.48%) | Memory: 14.3 MB (22.58%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        s = [i.lower() for i in s if i.isalpha() or i.isnumeric() ]
+        print(s)
+        i = 0 
+        j = len(s) - 1 
+
+        while i < j : 
+            if s[i] == s[j] :
+                i += 1 
+                j -= 1 
+            else :
+                return False 
+        return True
