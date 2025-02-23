af036ee2bdc0058146edf1da6a73141ad6e92705
Tuga Yousif <tugakariem@gmail.com>
2025-02-24 00:16:04 +0300

    Time: 143 ms (5.07%) | Memory: 12.3 MB (80.15%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
+        power_dict = {'0':0 , '1':1 , '2' :4 , '3':9 , '4' :16 , '5':25 ,
+                      '6':36  , '7':49 , '8':64 , '9':81}
+
+        ans = 0 
+        for i in range(1000) :
+            old_n = str(n)
+            # print("list",[power_dict[old_n[i]] for i in range(len(old_n))])
+            ans = sum([power_dict[old_n[i]] for i in range(len(old_n))])
+            # print("ans",ans)
+            if ans == 1 :
+                return True 
+            n = int(ans) 
+            # print("n",n) 
+        
+        return False
